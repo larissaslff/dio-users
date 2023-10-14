a0704483d0b09117c3a2a7a99fef5891b5d21349
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-14 17:22:32 -0300

    Initial commit

---
+++ dio-course/src/main/java/com/larissa/diocourse/DioCourseApplication.java
@@ -0,0 +1,13 @@
+package com.larissa.diocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCourseApplication.class, args);
+	}
+
+}
---
+++ dio-course/src/test/java/com/larissa/diocourse/DioCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.diocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
