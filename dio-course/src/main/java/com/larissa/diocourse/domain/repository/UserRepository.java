ab4717a220335501a569423002c5dd58abff72ba
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-14 19:49:36 -0300

    add models

---
+++ dio-course/src/main/java/com/larissa/diocourse/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.larissa.diocourse.domain.repository;
+
+import com.larissa.diocourse.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
